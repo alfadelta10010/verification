0d8720a5d05607364a0d0f975cf93c3d5db5f95f
Abhiram Dasika <alfadelta10010@gmail.com>
2023-09-01 09:18:50 +0530

    real and int limit test

---
+++ inttest.sv
@@ -0,0 +1,14 @@
+module test
+integer i1; // 0,1,x,z, default is x
+int i2; // 0 and 1, default is 0
+shortint i3;
+longint i4;
+
+initial
+	begin
+		$display("%b", i1); //xxxx xxxx xxxx xxxx xxxx xxxx xxxx xxxx
+		$display("%b", i2); //0000 0000 0000 0000 0000 0000 0000 0000
+		$display("%b", i3); // 
+		$display("%b", i4);
+	end 
+endmodule
---
+++ realtest.sv
@@ -0,0 +1,10 @@
+module test;
+real r5;
+shortreal r6;
+initial
+begin
+r5 = 6.777;
+r6 = 3.1;
+$display(r5, " ", r6);
+end
+endmodule
